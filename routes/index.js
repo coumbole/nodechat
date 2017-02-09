837a301d34db880692aeb9bbe97f7eaf86e7c541
coumbole <ville.kumpulainen@gmail.com>
2017-02-09 22:11:05 +0200

    Templates done, fixed gitignore and added required routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Nodechat' });
+});
+
+router.get('/chat', (req, res, next) => {
+	res.render('chat');
 });
 
 module.exports = router;
