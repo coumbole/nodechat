d7e87a63092cc398d21d010e8c518c9c2675798a
coumbole <ville.kumpulainen@gmail.com>
2017-02-10 15:59:00 +0200

    Logging in and out working

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var firebase = require('firebase')
+
+// Firebase configs
+var configs = require('../configs.json')
+
+firebase.initializeApp(configs.firebase)
+
+/*
+ *	Homepage routing etc
+ */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Nodechat' });
-});
+  res.render('index', { title: 'Nodechat' })
+})
 
-router.get('/chat', (req, res, next) => {
-	res.render('chat');
-});
 
-router.get('/login', (req, res, next) => {
-	res.render('login');
-});
+/*
+ *	Chat page routing
+ */
+router.get('/chat/', (req, res, next) => {
+	res.render('chat')
+})
+
+
+/*
+ * Login page routing and handling
+ */
+router.get('/login/', (req, res, next) => {
+	res.render('login')
+})
+
+// Handle logging in 
+router.post('/login/', (req, res, next) => {
+
+	firebase.auth().signInWithEmailAndPassword(
+		req.body.email, 
+		req.body.password)
+		.then( () => {
+			console.log("Sign in successful!")
+			res.redirect('/chat')
+		})
+		.catch( (error) => {
+			var ecode = error.code
+			var emsg = error.message
+			console.log("Error " + ecode + ": " + emsg)
+		})
+})
+
+/*
+ *	Hande logging out
+ */
+router.get('/logout/', (req, res, next) => {
+	firebase.auth().signOut().then( () => {
+		console.log("Successfully signed out")
+		res.redirect('/')
+	})
+	.catch( (error) => {
+		console.log("Error while signing out")
+		res.redirect('/')
+	})
+})
+
+
+/*
+ *	Handling registering
+ */
 
 router.get('/register', (req, res, next) => {
 	res.render('register');
