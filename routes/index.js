5312ae55a1d4962e33652d207e834d02ed696567
coumbole <ville.kumpulainen@gmail.com>
2017-02-10 21:43:05 +0200

    Migrated project's template engine from jade to pug

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 var firebase = require('firebase')
 
@@ -11,7 +11,7 @@ firebase.initializeApp(configs.firebase)
 /*
  *	Homepage routing etc
  */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
 	var user = firebase.auth().currentUser
 	res.render('index', { user: user })
 })
@@ -20,7 +20,7 @@ router.get('/', (req, res, next) => {
 /*
  *	Chat page routing
  */
-router.get('/chat/', (req, res, next) => {
+router.get('/chat/', (req, res) => {
 	res.render('chat')
 })
 
@@ -28,37 +28,37 @@ router.get('/chat/', (req, res, next) => {
 /*
  * Login page routing and handling
  */
-router.get('/login/', (req, res, next) => {
+router.get('/login/', (req, res) => {
 	res.render('login')
 })
 
 // Handle logging in 
-router.post('/login/', (req, res, next) => {
+router.post('/login/', (req, res) => {
 
 	firebase.auth().signInWithEmailAndPassword(
 		req.body.email, 
 		req.body.password)
 		.then( () => {
-			console.log("Sign in successful!")
+			console.log('Sign in successful!')
 			res.redirect('/chat')
 		})
 		.catch( (error) => {
 			var ecode = error.code
 			var emsg = error.message
-			console.log("Error " + ecode + ": " + emsg)
+			console.log('Error ' + ecode + ': ' + emsg)
 		})
 })
 
 /*
  *	Hande logging out
  */
-router.get('/logout/', (req, res, next) => {
+router.get('/logout/', (req, res) => {
 	firebase.auth().signOut().then( () => {
-		console.log("Successfully signed out")
+		console.log('Successfully signed out')
 		res.redirect('/')
 	})
 	.catch( (error) => {
-		console.log("Error while signing out")
+		console.log('Error while signing out: ' + error)
 		res.redirect('/')
 	})
 })
@@ -68,8 +68,8 @@ router.get('/logout/', (req, res, next) => {
  *	Handling registering
  */
 
-router.get('/register', (req, res, next) => {
-	res.render('register');
-});
+router.get('/register', (req, res) => {
+	res.render('register')
+})
 
-module.exports = router;
+module.exports = router
