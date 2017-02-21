bd1ac4741db55e6f44fcc61f34420ff138ac7728
coumbole <ville.kumpulainen@gmail.com>
2017-02-21 10:36:13 +0200

    Settings drawer opening correctly

---
+++ public/javascripts/settings.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+  $('#user-settings').click( (e) => {
+    $('#user-nav').css('width', () => {
+      return $('.chats').css('width')
+    })
+  })
+
+  $('#closebtn').click( (e) => {
+    $('#user-nav').css('width', '0')
+  })
+/* eslint-enable */
