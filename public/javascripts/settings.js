81e194a365ee53c4d9ae3005fde2c3fa738e9d4a
coumbole <ville.kumpulainen@gmail.com>
2017-02-26 22:29:49 +0200

    Public chats can now be searched and filtered by text

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,29 +1,38 @@
 $(document).ready( () => {
   $('#user-settings').click( () => {
     $('#user-nav').css('width', () => {
-      return $('.chats').css('width')
-    })
-  })
+      return $('.chats').css('width');
+    });
+  });
 
   $('#closebtn').click( () => {
-    $('#user-nav').css('width', '0')
-  })
+    $('#user-nav').css('width', '0');
+  });
 
   $('#searchfield').focus(() => {
-    $('.public-chats').css('width', () => {
-      return $('.chats').css('width')
-    })
-  })
+    $('.public-chats').show();
+  });
 
   $('#searchfield').focusout( () => {
-    $('.public-chats').css('width', '0')
-  })
+    $('.public-chats').hide();
+  });
 
+  var data;
   $('#searchfield').on('keyup', (e) => {
-    var value = e.target.value.toLowerCase()
+    var value = e.target.value.toLowerCase();
+    if (value === '') $('.public-chats').append(data);
+    $('.public-chats .chat').each( (i, v) => {
+      $(v).toggle( $(v).text().toLowerCase().indexOf(value) !== -1 );
+      //$(v).toggle( $(v).text().toLowerCase().indexOf(value) !== -1 );
+    });
+  });
+  /*
+  $('#searchfield').on('keyup', (e) => {
+    var value = e.target.value.toLowerCase();
     $('.public-chats .chat .chat-title h4').each( (i, v) => {
-      $(v).toggle( $(v).text().toLowerCase().indexOf(value) !== -1 )
-    })
-  })
-})
+      $(v).toggle( $(v).text().toLowerCase().indexOf(value) !== -1 );
+    });
+  });
+  */
+});
 
