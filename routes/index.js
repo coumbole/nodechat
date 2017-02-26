69b86e8fdf99df79c2a09cf9d7536e66e5ea60d7
coumbole <ville.kumpulainen@gmail.com>
2017-02-26 21:36:10 +0200

    When joining a chatroom, the subscription is saved to firebase and remembered for the next time

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,31 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
-var firebase = require('firebase')
+var firebase = require('firebase');
 
 // Firebase configs
-var configs = require('../configs.json')
+var configs = require('../configs.json');
 
-firebase.initializeApp(configs.firebase)
+firebase.initializeApp(configs.firebase);
 
-var dbref = firebase.database().ref()
+var dbref = firebase.database().ref();
 
 /**
  * Middleware to ensure only authenticated users can chat
  */
 function isAuthenticated(req, res, next) {
-  var user = firebase.auth().currentUser
-  if (user) return next()
-  else res.redirect('/login')
+  var user = firebase.auth().currentUser;
+  if (user) return next();
+  else res.redirect('/login');
 }
 
 /*
  *  Homepage routing etc
  */
 router.get('/', (req, res) => {
-  var user = firebase.auth().currentUser
-  res.render('index', { user: user })
-})
+  var user = firebase.auth().currentUser;
+  res.render('index', { user: user });
+});
 
 
 /*
@@ -35,54 +35,64 @@ router.get('/', (req, res) => {
  *  between '/chat/' and (req, res)
  */
 router.get('/chat/', isAuthenticated, (req, res) => {
-  var user = firebase.auth().currentUser
+  var user = firebase.auth().currentUser;
 
-  console.log('Current user is ' + user.email)
+  console.log('Current user is ' + user.email);
 
-  var nick = null
-  var chats = null
-  var rooms = null
+  var nick = null;
+  var chats = null;
+  var rooms = null;
   
   dbref.once('value').then( (snapshot) => {
 
     // Try to set the nick and get user's rooms
     try {
-      nick = snapshot.child('users/' + user.uid + '/nickname').val()
-      rooms = snapshot.child('users/' + user.uid + '/rooms').val()
+      nick = snapshot.child('users/' + user.uid + '/nickname').val();
+      rooms = snapshot.child('users/' + user.uid + '/rooms').val();
     } catch(e) {
-      console.log('error: ' + e)
-      console.log('defaulting to anon username')
-      nick = 'anon'
-      rooms = {global: 'global'}
+      console.log('error: ' + e);
+      console.log('defaulting to anon username');
+      nick = 'anon';
+      rooms = {global: 'global'};
     }
 
     // Then fetch chatdata
-    chats = snapshot.child('chat').val()
-    var chatrooms = {} 
+    chats = snapshot.child('chat').val();
 
+    // Fetch the user's own chatrooms
+    var chatrooms = {} ;
     for (var chat in chats) {
       for (var room in rooms) {
         if (chat === room) {
-          chatrooms[chat] = chats[chat]
+          chatrooms[chat] = chats[chat];
         }
+      }
+    }
+
+    // Fetch public chatrooms
+    var pub_rooms = {};
+    for (var c in chats) {
+      if (chats[c].public === true) {
+        pub_rooms[c] = chats[c];
       }
     }
 
     res.render('chat', {
       'chats': chatrooms,
+      'pubs': pub_rooms,
       'nickname': nick,
       'user': user
-    })
-  })
-})
+    });
+  });
+});
 
 
 /*
  * Login page routing and handling
  */
 router.get('/login/', (req, res) => {
-  res.render('login')
-})
+  res.render('login');
+});
 
 // Handle logging in 
 router.post('/login/', (req, res) => {
@@ -91,29 +101,29 @@ router.post('/login/', (req, res) => {
     req.body.email, 
     req.body.password)
     .then( () => {
-      console.log('Sign in successful!')
-      res.redirect('/chat')
+      console.log('Sign in successful!');
+      res.redirect('/chat');
     })
     .catch( (error) => {
-      var ecode = error.code
-      var emsg = error.message
-      console.log('Error ' + ecode + ': ' + emsg)
-    })
-})
+      var ecode = error.code;
+      var emsg = error.message;
+      console.log('Error ' + ecode + ': ' + emsg);
+    });
+});
 
 /*
  *  Hande logging out
  */
 router.get('/logout/', (req, res) => {
   firebase.auth().signOut().then( () => {
-    console.log('Successfully signed out')
-    res.redirect('/')
+    console.log('Successfully signed out');
+    res.redirect('/');
   })
   .catch( (error) => {
-    console.log('Error while signing out: ' + error)
-    res.redirect('/')
-  })
-})
+    console.log('Error while signing out: ' + error);
+    res.redirect('/');
+  });
+});
 
 
 /*
@@ -121,7 +131,7 @@ router.get('/logout/', (req, res) => {
  */
 
 router.get('/register', (req, res) => {
-  res.render('register')
-})
+  res.render('register');
+});
 
-module.exports = router
+module.exports = router;
